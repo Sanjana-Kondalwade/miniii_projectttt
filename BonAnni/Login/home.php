3657d00750bdc2cf77fa266d7e0f925953f080d3
Sanjana <s20_kondalwade_sanjana@mgmcen.ac.in>
2022-12-04 15:58:25 +0530

    Add files via upload

---
+++ BonAnni/Login/home.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Bon Anniversaire!</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Bon Anniversaire </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+  <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="register.php">Register</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="login.php">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="logout.php">Logout</a>
+      </li>
+  </ul>
+  </div>
+</nav>
+    <div class="top_container">
+        Generate a Personalized Wish<br>
+        for Birthday !
+    </div>
+    <div class="mid_container">
+        Are you bored with sending birthday wishes to your friends ?<br> or do you forget to send wishes to your
+         friends ?<br> or do you want to wish them at 12 AM but you always fall asleep?<br> Why not automate this simple task...
+    </div><br><br><br><br><br>
+<hr>
+<div class="contact_us">
+<a class="btn" href="mailto:kondalwadesanjana@gmail.com">CONTACT ME</a>
+</div>
+
+<div class="bottom_container">
+
+</div>
+
+  </body>
+</html>
